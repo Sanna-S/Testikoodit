d8b178f05f8b0ccf7560a9c847cf08d7a14d7d9c
Sanna-S <sanna.sjoblad@centria.fi>
2025-11-12 16:31:31 +0200

    eka

---
+++ syksy2025.py
@@ -0,0 +1,9 @@
+def ekafunktio():
+    print("Tämä on ensimmäinen funktio.")
+
+def main():
+    print("Tervetuloa syksyn 2025 ohjelmaan!")
+    ekafunktio()
+
+if __name__ == "__main__":
+     main()
