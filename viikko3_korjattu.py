8dc5ed4eeeac5dacd898041296cf0c08f850fca8
Sanna-S <sanna.sjoblad@centria.fi>
2025-11-18 10:51:13 +0200

    korjattu

--- viikko3_korjattu.py
+++ viikko3_korjattu.py
@@ -4,7 +4,6 @@ def hae_varausnumero(varaus):
     varausnumero = int(varaus[0])
     print(f"Varausnumero: {varausnumero}")
     
-
 def hae_varaaja(varaus):
     nimi = str(varaus[1])
     print(f"Varaaja: {nimi}")
@@ -14,28 +13,25 @@ def hae_paiva(varaus):
     suomalainenpvm = paivamaara.strftime("%d.%m.%Y")
     print(f"Päivämäärä: {suomalainenpvm}")
     
-
 def hae_aloitusaika(varaus):
     aloitusaika = datetime.strptime(varaus[3], "%H:%M").time()
     suomalainenaika = aloitusaika.strftime("%H.%M")
     print(f"Aloitusaika: {suomalainenaika}")
        
-    
 def hae_tuntimaara(varaus):
     tuntimaara = int(varaus[4])
     print(f"Tuntimäärä: {tuntimaara}")
     
- 
 def hae_tuntihinta(varaus):
     tuntihinta = float(varaus[5])
     print(f"Tuntihinta: {tuntihinta:.2f}".replace('.',',') + " €")
   
 def laske_kokonaishinta(varaus):
-    kokonainshinta = int(varaus[4]) * float(varaus[5])
-    print(f"Kokonaishinta: {kokonainshinta:.2f}".replace('.',',') + " €")
+    kokonaishinta = int(varaus[4]) * float(varaus[5])
+    print(f"Kokonaishinta: {kokonaishinta:.2f}".replace('.',',') + " €")
     
 def hae_maksettu(varaus):
-    maksettu = bool(varaus[6])
+    maksettu = varaus[6]
     print(f"Maksettu: {'Kyllä' if maksettu else 'Ei'}")
 
 def hae_kohde(varaus):
@@ -50,13 +46,24 @@ def hae_sahkoposti(varaus):
     sahkoposti = str(varaus[9])
     print(f"Sähköposti: {sahkoposti}")
     
-
 def main():
     varaukset = "varaukset.txt"
 
     with open(varaukset, "r", encoding="utf-8") as f:
         varaus = f.read().strip()
         varaus = varaus.split('|')
+
+    hae_varausnumero(varaus)
+    hae_varaaja(varaus)
+    hae_paiva(varaus)
+    hae_aloitusaika(varaus)
+    hae_tuntimaara(varaus)
+    hae_tuntihinta(varaus)
+    laske_kokonaishinta(varaus)
+    hae_maksettu(varaus)
+    hae_kohde(varaus)
+    hae_puhelin(varaus)
+    hae_sahkoposti(varaus)
     
 if __name__ == "__main__":
     main()
